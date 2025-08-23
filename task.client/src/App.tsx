1b8793a627b0a11ae74e0702880ff5756e618e2c
EdvinasTo <edvinas.toliusis1@gmail.com>
2025-08-23 23:49:31 +0300

    remove a comment

--- task.client/src/App.tsx
+++ task.client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
     return (
         <QueryClientProvider client={queryClient}>
-            <SnackbarProvider> {/* Wrap with your provider */}
+            <SnackbarProvider>
                 <Navbar />
                 <Outlet />
             </SnackbarProvider>
