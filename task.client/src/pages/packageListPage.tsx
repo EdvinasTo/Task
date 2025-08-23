6f4a70169189cfa228f86f0dde51f98231cf1e55
EdvinasTo <edvinas.toliusis1@gmail.com>
2025-08-23 17:33:31 +0300

    connect api to retrieve package list

--- task.client/src/pages/packageListPage.tsx
+++ task.client/src/pages/packageListPage.tsx
@@ -1,15 +1,50 @@
+import { useEffect, useState } from 'react';
 import CustomButton from '../components/button';
-import Navbar from '../components/navbar';
 import PackageListTable from '../components/packagesTable';
+import { packagesApi } from '../api/packagesApi';
+import type { Package } from '../types/package';
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
 
 function PackageListPage() {
+    const [packages, setPackages] = useState<Package[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchPackages = async () => {
+            try {
+                const data = await packagesApi.getAllPackages();
+                setPackages(data);
+            } catch (err) {
+                setError("Failed to load packages.");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPackages();
+    }, []);
+
+    
+
+    const handleCreatePackage = () => {
+        navigate('/create');
+    };
+
     return (
         <>
             <h1>Package List</h1>
-            <PackageListTable rows={[]} />
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p style={{ color: "red" }}>{error}</p>
+            ) : (
+                <PackageListTable rows={packages} />
+            )}
             <div className="button-container">
-                <CustomButton label={'Create Package'} />
+                <CustomButton label={'Create Package'} onClick={handleCreatePackage} />
             </div>
         </>
     );
